4f92fee6ca6453be4c3dcce3910ceacf838d9807
Connor Clark <c.clark000@hotmail.com>
2020-04-17 10:00:50 -0500

    minor code cleanup

--- monsewn/monsewn.cpp
+++ monsewn/monsewn.cpp
@@ -1,28 +1,25 @@
-#include <iostream>
 #include <boost/program_options.hpp>
+#include <iostream>
 
 namespace po = boost::program_options;
 
 int main(int argc, char* argv[])
 {
-    try
-    {
+    try {
         po::options_description desc{"Options"};
         desc.add_options()
-            ("help,h", "Help");
+            ("help,h", "print usage")
+            ;
 
         po::variables_map vm;
         store(parse_command_line(argc, argv, desc), vm);
-        notify(vm);
 
-        if (vm.count("help")) 
-        {
+        if (vm.count("help")) {
             std::cout << desc << std::endl;
             return 0;
         }
-    }
-    catch (const po::error& ex)
-    {
+
+    } catch (const std::exception& ex) {
         std::cerr << ex.what() << std::endl;
         return 1;
     }
