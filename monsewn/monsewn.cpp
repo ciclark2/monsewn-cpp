da94f21fb3444446ee1988bc8e7dcce248d7679e
Connor Clark <c.clark000@hotmail.com>
2020-04-17 09:45:22 -0500

    pulling in boost from conan

--- monsewn/monsewn.cpp
+++ monsewn/monsewn.cpp
@@ -1,8 +1,31 @@
 #include <iostream>
+#include <boost/program_options.hpp>
+
+namespace po = boost::program_options;
 
 int main(int argc, char* argv[])
 {
-	std::cout << "success" << std::endl;
-	std::getchar();
-	return 0;
+    try
+    {
+        po::options_description desc{"Options"};
+        desc.add_options()
+            ("help,h", "Help");
+
+        po::variables_map vm;
+        store(parse_command_line(argc, argv, desc), vm);
+        notify(vm);
+
+        if (vm.count("help")) 
+        {
+            std::cout << desc << std::endl;
+            return 0;
+        }
+    }
+    catch (const po::error& ex)
+    {
+        std::cerr << ex.what() << std::endl;
+        return 1;
+    }
+
+    return 0;
 }
